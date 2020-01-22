f998032d7b87f7ca7f681046a4050fc02dfff010
Gurdeep <gurdeep.singh@quovantis.com>
2020-01-22 16:57:59 +0530

    express routing added

--- Express/main.js
+++ Express/main.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 
 app.get("/", (eq, res) => res.send("Hello world"));
-app.get("/users", (eq, res) => res.send("Hello users"));
+
+app.get("/users/:id?", (req, res) => {
+    req.params.id ? res.send(`Hello user ${req.params.id}`) : res.send(`Hello users`)
+});
 
 app.listen("5000", () => console.log("server started"));
 
